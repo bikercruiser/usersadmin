03e3279b8799a756d708fa21dbb8429a28c4c554
Khitrov Aleksandr <khitr@DESKTOP-VLOC903>
2019-03-02 20:25:01 +0300

    Add Edit modal window

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
 //require_once 'config/config.php';
-require 'include/classes/Db.class.php';
+//require 'include/classes/Db.class.php';
+spl_autoload_register(function ($class_name) {
+    include $_SERVER['DOCUMENT_ROOT'] . '/include/classes/' . $class_name . '.class.php';
+});
 
+/*
 $cls = new Db();
 $cls->connect();
+ * 
+ */
+require_once 'views/header.php';
+require_once 'views/main.php';
+
+require_once 'views/footer.php';
 ?>
