4e1d166a224b8c5594528af398786c8f51fee5b2
Khitrov Aleksandr <khitr@LAPTOP-UDFC4UUQ>
2019-03-03 01:42:56 +0300

    Refactor footer and main views

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,3 @@
-</div>
 <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
 <script src="js/popper.min.js" type="text/javascript"></script>
 <script src="js/bootstrap.min.js" type="text/javascript"></script>
--- views/main.php
+++ views/main.php
@@ -1,38 +1,38 @@
-<body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Users admin</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <!--<a class="p-2 text-dark" href="#">Features</a>
-            <a class="p-2 text-dark" href="#">Enterprise</a>
-            <a class="p-2 text-dark" href="#">Support</a>
-            <a class="p-2 text-dark" href="#">Pricing</a>-->
-        </nav>
-        <a class="btn btn-outline-primary" href="#">Log out</a>
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Users admin</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <!--<a class="p-2 text-dark" href="#">Features</a>
+        <a class="p-2 text-dark" href="#">Enterprise</a>
+        <a class="p-2 text-dark" href="#">Support</a>
+        <a class="p-2 text-dark" href="#">Pricing</a>-->
+    </nav>
+    <a class="btn btn-outline-primary" href="#">Log out</a>
+</div>
+<div class="container">
+    <div class="p-3">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addUser">Добавить</button>
     </div>
-    <div class="container">
-        <div class="p-3">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addUser">Добавить</button>
-        </div>
-        <div id="refreshId">
-            <form id="userTableForm">
-                <table class="table" id="userTable">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>ФИО</th>
-                            <th>E-Mail</th>
-                            <th>Адрес</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody id="userTableBody"></tbody>
-                </table>
-                <div class="p-3">
-                    <button type="submit" class="btn btn-danger" onclick="return confirm('Вы уверены, что хотите удалить пользователя?');">Удалить</button>
-                </div>
-            </form>
-        </div>
-        <?php
-        require_once 'views/addform.php';
-        require_once 'views/editform.php';
-        ?>
+    <div id="refreshId">
+        <form id="userTableForm">
+            <table class="table" id="userTable">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>ФИО</th>
+                        <th>E-Mail</th>
+                        <th>Адрес</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody id="userTableBody"></tbody>
+            </table>
+            <div class="p-3">
+                <button type="submit" class="btn btn-danger" onclick="return confirm('Вы уверены, что хотите удалить пользователя?');">Удалить</button>
+            </div>
+        </form>
+    </div>
+    <?php
+    require_once 'views/addform.php';
+    require_once 'views/editform.php';
+    ?>
+</div>
